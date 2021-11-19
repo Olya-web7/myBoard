c3d88ec78c2220a8fcff9b3dd38e98620d772c1d
Olya <shyshova24@gmail.com>
2021-11-19 18:34:25 +0200

    g c header html

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
     HeaderComponent
