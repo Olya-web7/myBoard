05b82ef78f254a90f871df9ee33bfdfdb1949649
Olya <shyshova24@gmail.com>
2021-11-19 18:49:33 +0200

    board

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from './board.component';
 
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     BoardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    DragDropModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatInputModule
   ],
   exports: [
     BoardComponent
