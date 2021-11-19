cbb05506c81796289540667fa94c51498f7c9de8
Olya <shyshova24@gmail.com>
2021-11-19 19:00:46 +0200

    drop-drag

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
@@ -11,5 +12,27 @@ export class BoardComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  lists = [
+    this.done,
+    this.todo
+  ]
 
 }
