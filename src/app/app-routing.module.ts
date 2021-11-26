98c2ae41430618eb5d4c1eff5b54504941bc9c02
Olya <shyshova24@gmail.com>
2021-11-26 15:57:44 +0200

    signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { BoardComponent } from './board/board.component';
 
 const routes: Routes = [
   { path: '', component: BoardComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
